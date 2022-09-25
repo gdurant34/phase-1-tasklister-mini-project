e32a43b4469bcbfc07293f0664df95f52a438904
Grace Durant <gdurant34@gmail.com>
2022-09-25 10:47:56 -0700

    Updated Completed Deliverales

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let form = document.querySelector('#create-task-form')
-  form.addEventListener('submit', (e) => {
+document.addEventListener('DOMContentLoaded', () => {
+  let form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', e => {
     e.preventDefault();
-    buildToDo(e.target["new-task-description"].value);
+    console.log(e.target[2].value);
+    const priority = (e.target[2].value);
+    const input = e.target['new-task-description'].value;
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    const ul = document.querySelector('#tasks');
+
+    btn.addEventListener('click', e => {
+      e.target.parentNode.remove();
+    });
+    btn.textContent = "X";
+    li.textContent = `${input} `;
+    li.className = priority.toLowerCase();
+    ul.appendChild(li);
+    li.appendChild(btn);
     form.reset();
   });
+  const select = document.createElement('select');
+  const options = ['Select Priority', 'High', 'Medium', 'Low'];
+
+  options.forEach(option => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = option;
+    select.appendChild(optionElement);
+  });
+
+  form.appendChild(select);
+  console.log(form);
 });
-
-function buildToDo(todo) {
-  console.log(todo);
-  let p = document.createElement('p');
-  let btn = document.createElement('button');
-  p.textContent = `${todo} `;
-  btn.textContent = 'x';
-  btn.addEventListener('click', handleDelete);
-  p.appendChild(btn);
-  document.querySelector('#list').appendChild(p);
-};
-
-function handleDelete(e) {
-  e.target.parentNode.remove();
-};
-
-
